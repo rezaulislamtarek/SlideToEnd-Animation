cdd5b7d203e7fffb7cc6f35fdbfb65b7d2034bcc
rezaulislamtarek <mrezaulislamtarek.t@gmail.com>
2024-02-23 15:51:11 +0600

    Initial Commit

---
+++ SlideToEnd Animation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SlideToEnd Animation
+//
+//  Created by Rezaul Islam on 23/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SlideToEnd Animation/SlideToEnd_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideToEnd_AnimationApp.swift
+//  SlideToEnd Animation
+//
+//  Created by Rezaul Islam on 23/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SlideToEnd_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
